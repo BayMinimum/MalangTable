d06007b264f40ad8fb0736a88d72f991d1040932
BayMinimum <bearksb1115@gmail.com>
2017-08-15 13:56:18 +0900

    Implement mechanism for retrieving timetable HTML from students web

--- app/src/main/java/me/baymallow/malangtable/DownloadActivity.kt
+++ app/src/main/java/me/baymallow/malangtable/DownloadActivity.kt
@@ -3,21 +3,48 @@ package me.baymallow.malangtable
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 
 import kotlinx.android.synthetic.main.activity_download.*
+import kotlinx.android.synthetic.main.content_download.*
 
 class DownloadActivity : AppCompatActivity() {
+    lateinit var mChromeClient: MyChromeClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        mChromeClient = MyChromeClient()
+
+        download_fab.setOnClickListener {
+            download_webview.loadUrl("http://students.ksa.hs.kr/scmanager/stuweb/kor/sukang/state.jsp")
+            mChromeClient.flag = true
+        }
+
+        download_webview.settings.javaScriptEnabled = true
+        download_webview.webChromeClient = mChromeClient
+        download_webview.loadUrl("http://students.ksa.hs.kr")
+
+        Snackbar.make(download_fab, R.string.press_after_login, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, {}).show()
+
+    }
+
+}
+
+class MyChromeClient : WebChromeClient() {
+    var flag = false
+
+    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+        super.onProgressChanged(view, newProgress)
+        if (flag) {
+            TODO("Get timetable data from HTML")
+        }
     }
 
 }
