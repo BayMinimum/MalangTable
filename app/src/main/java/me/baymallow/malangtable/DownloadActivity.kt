6ca0db2ca45f28a0cfc91d6dad70949103d67deb
BayMinimum <bearksb1115@gmail.com>
2017-08-15 22:04:09 +0900

    Optimize imports

--- app/src/main/java/me/baymallow/malangtable/DownloadActivity.kt
+++ app/src/main/java/me/baymallow/malangtable/DownloadActivity.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
 import android.webkit.WebView
--- app/src/main/java/me/baymallow/malangtable/MainActivity.kt
+++ app/src/main/java/me/baymallow/malangtable/MainActivity.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Toast
