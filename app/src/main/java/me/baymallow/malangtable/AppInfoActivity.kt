3879da0ced7c76894822ab05d6e3e906eb3d247e
BayMinimum <bearksb1115@gmail.com>
2017-08-15 23:35:04 +0900

    Add up action support for AppInfoActivity

--- app/src/main/java/me/baymallow/malangtable/AppInfoActivity.kt
+++ app/src/main/java/me/baymallow/malangtable/AppInfoActivity.kt
@@ -8,5 +8,7 @@ class AppInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
+
+
     }
 }
